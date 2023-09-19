bcf1d5e545ff0e2d6583c889ba4f7dcecf828273
Abiola17 <josephsegun696@gmail.com>
2023-09-19 16:11:39 -0700

    Update

--- _printf.c
+++ _printf.c
@@ -4,40 +4,40 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format.
+ * @fmt: fmt.
  * Return: Printed chars.
  */
-int _printf(const char *format, ...)
+int _printf(const char *fmt, ...)
 {
-	int i, printed = 0, printed_chars = 0;
+	int index, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_index = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
-	if (format == NULL)
+	if (fmt == NULL)
 		return (-1);
 
-	va_start(list, format);
+	va_start(list, fmt);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (index = 0; fmt && fmt[index] != '\0'; index++)
 	{
-		if (format[i] != '%')
+		if (fmt[index] != '%')
 		{
-			buffer[buff_index++] = format[i];
+			buffer[buff_index++] = fmt[index];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_index);
-			/* write(1, &format[i], 1);*/
+			/* write(1, &fmt[index], 1);*/
 			printed_chars++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_index);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
+			flags = getFlags(fmt, &index);
+			width = getWidth(fmt, &index, list);
+			precision = getPrecision(fmt, &index, list);
+			size = getSize(fmt, &index);
+			++index;
+			printed = handlePrint(fmt, &index, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
