e71be692f4021f002667ed42c990485f46f08723
Abiola17 <josephsegun696@gmail.com>
2023-09-21 11:13:33 -0700

    Checking

--- _printf.c
+++ _printf.c
@@ -8,10 +8,9 @@ int _printf(const char *format, ...)
         return -1; // Error: NULL format string
     }
 
+    int char_count = 0;
     va_list args;
     va_start(args, format);
-
-    int char_count = 0;
 
     while (*format) {
         if (*format == '%') {
